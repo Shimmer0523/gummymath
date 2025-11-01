76a0c0e39febb4ff29042fdb3e0f81455db32f08
shimmer0523 <androcoffee@gmail.com>
2025-11-01 09:57:00 +0900

    change pointer to copies

--- vector2d.go
+++ vector2d.go
@@ -9,47 +9,47 @@ type Vector2D struct {
 }
 
 // New creates a new 2D vector.
-func NewVector2D(x, y float64) *Vector2D {
-	return &Vector2D{X: x, Y: y}
+func NewVector2D(x, y float64) Vector2D {
+	return Vector2D{X: x, Y: y}
 }
 
 // Add adds two vectors and returns a new vector.
-func (v *Vector2D) Add(other *Vector2D) *Vector2D {
-	return &Vector2D{X: v.X + other.X, Y: v.Y + other.Y}
+func (v Vector2D) Add(other Vector2D) Vector2D {
+	return Vector2D{X: v.X + other.X, Y: v.Y + other.Y}
 }
 
 // Sub subtracts two vectors and returns a new vector.
-func (v *Vector2D) Sub(other *Vector2D) *Vector2D {
-	return &Vector2D{X: v.X - other.X, Y: v.Y - other.Y}
+func (v Vector2D) Sub(other Vector2D) Vector2D {
+	return Vector2D{X: v.X - other.X, Y: v.Y - other.Y}
 }
 
 // Scale scales a vector by a scalar and returns a new vector.
-func (v *Vector2D) Scale(s float64) *Vector2D {
-	return &Vector2D{X: v.X * s, Y: v.Y * s}
+func (v Vector2D) Scale(s float64) Vector2D {
+	return Vector2D{X: v.X * s, Y: v.Y * s}
 }
 
 // Dot calculates the dot product of two vectors.
-func (v *Vector2D) Dot(other *Vector2D) float64 {
+func (v Vector2D) Dot(other Vector2D) float64 {
 	return v.X*other.X + v.Y*other.Y
 }
 
 // Cross calculates the cross product of two vectors.
 // The cross product of 2D vectors is a scalar.
-func (v *Vector2D) Cross(other *Vector2D) float64 {
+func (v Vector2D) Cross(other Vector2D) float64 {
 	return v.X*other.Y - v.Y*other.X
 }
 
 // Rotate rotates a vector by a given angle in radians.
-func (v *Vector2D) Rotate(angle float64) *Vector2D {
+func (v Vector2D) Rotate(angle float64) Vector2D {
 	cos := math.Cos(angle)
 	sin := math.Sin(angle)
-	return &Vector2D{
+	return Vector2D{
 		X: v.X*cos - v.Y*sin,
 		Y: v.X*sin + v.Y*cos,
 	}
 }
 
 // Length returns the length (magnitude) of the vector.
-func (v *Vector2D) Length() float64 {
+func (v Vector2D) Length() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
